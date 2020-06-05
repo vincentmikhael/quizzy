5b99eb082c7182a74a496e90c02a419bad3b9500
vincentmikhael <vincentmikhael24@gmail.com>
2020-06-05 12:58:12 +0700

    commit

---
+++ precache-manifest.0f8602ca0550b8921613f95da13c66ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bd573663e213eccb829043b1044126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47265d9be50f374aa18c",
+    "url": "/static/css/main.b3e77220.chunk.css"
+  },
+  {
+    "revision": "8f8c4e63e7929859c6a3",
+    "url": "/static/js/2.0871570b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0871570b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47265d9be50f374aa18c",
+    "url": "/static/js/main.bd64cc7e.chunk.js"
+  },
+  {
+    "revision": "e5c703b5361ea9a63e18",
+    "url": "/static/js/runtime-main.d7f04d76.js"
+  }
+]);
